47151898b3a50cea7fda4484f2e9d0eabecd441d
obixander <noahhauge@gmail.com>
2024-05-29 09:20:43 +0200

    Add .net maui app

---
+++ SosuCentre.API/SosuCentre.CareApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SosuCentre.CareApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SosuCentre.API/SosuCentre.CareApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SosuCentre.CareApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
